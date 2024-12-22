f20d6a09971b5f7c6cb3d81b76489324b88a7a69
yosmisyael <yosmisyael7308@gmail.com>
2024-12-22 11:26:06 +0700

    feat: pass unit like use case test

--- src/Applications/use_case/LikeUseCase.js
+++ src/Applications/use_case/LikeUseCase.js
@@ -1,4 +1,3 @@
-
 class LikeUseCase {
     constructor({ likeRepository, threadRepository, commentRepository }) {
         this._likeRepository = likeRepository;
@@ -9,7 +8,17 @@ class LikeUseCase {
     }
 
     async execute(userId, threadId, commentId) {
+        await this._threadRepository.checkAvailableThread(threadId);
 
+        await this._commentRepository.checkAvailableComment(commentId);
+
+        const likeExists = await this._likeRepository.verifyLikeExists(userId, commentId);
+
+        if (likeExists) {
+            return this._likeRepository.deleteLike(userId, commentId);
+        }
+
+        return this._likeRepository.addLike(userId, commentId);
     }
 }
 
