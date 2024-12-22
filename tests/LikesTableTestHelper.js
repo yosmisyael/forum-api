4056aaf1233e19ae3c2330a2ba04899f1a6ee3a7
yosmisyael <yosmisyael7308@gmail.com>
2024-12-22 12:19:55 +0700

    fix: fixed error path for pool object

--- tests/LikesTableTestHelper.js
+++ tests/LikesTableTestHelper.js
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
-const pool = require('src/Infrastructures/database/postgres/pool');
+const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const LikesTableTestHelper = {
     async addLike({
